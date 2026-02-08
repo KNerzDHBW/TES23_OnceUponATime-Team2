eae079d8b015d0c452946a064f57692dfe66b4ad
KNerzDHBW <131165020+KNerzDHBW@users.noreply.github.com>
2026-02-08 11:39:53 +0000

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,67 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        /*
+         * Buchtitel
+         */
+        System.out.println();
+        System.out.println("Die Maerchen von TES23 -- Band 1");
+        System.out.println();
+
+        /*
+         * Erstes Kapitel
+         */
+        System.out.println();
+        System.out.println("Die Maus, der Vogel und die Bratwurst");
+        System.out.println();
+
+        System.out.println("Es waren einmal eine Maus, ein Vogel und ein Bratwurst, die lebten ");
+        System.out.println("zusammen in einer kleinen Huette am Rande eines verwunschenen Waldes.");
+        System.out.println();
+
+        /*
+         * Zweites Kapitel
+         */
+        System.out.println();
+        System.out.println("Der Prinz und der Frosch");
+        System.out.println();
+        System.out.println("Vor langer Zeit gab es einmal einen magischen See, der war so ");
+        System.out.println("besonders, dass er nur von denjenigen gefunden werden konnte, ");
+        System.out.println("von denen der See sich finden lassen wollte.");
+        System.out.println();
+
+        /*
+         * Drittes Kapitel
+         */
+        System.out.println();
+        System.out.println("Die sieben Zwerge und der Drache");
+        System.out.println();
+
+        System.out.println("Vor langer langer Zeit lebte einst ein kleines Maedchen, das alle ");
+        System.out.println("Rotkaeppchen nannten, denn es trug immer ein feuerrotes Maentelchen.");
+        System.out.println();
+
+        /*
+         * Viertes Kapitel
+         */
+        System.out.println();
+        System.out.println("Der verlorene Schatz");
+        System.out.println();
+
+        System.out.println("Es lebte einmal in einem weit entfernten Land ein Koenig und ");
+        System.out.println("eine Koenigin, die wuenschten sich nichts sehnlicher als nicht ");
+        System.out.println("mehr Koenig und Koenigin sein zu muessen.");
+        System.out.println();
+
+        /*
+         * Fünftes Kapitel
+         */
+        System.out.println();
+        System.out.println("Das Traumfresserchen");
+        System.out.println();
+        System.out.println("Es war einmal ein Koenigreich, in dem war seit einem Jahr die ");
+        System.out.println("Sonne nicht mehr aufgegangen.");
+        System.out.println();
+    }
+}

7cfe28d20151253c68255e3ba7459c23caf0702b
ninaknott <153294920+ninaknott@users.noreply.github.com>
2026-02-27 10:36:59 +0000

    Änderungen im Kapitel 1

--- src/main.java
+++ src/main.java
@@ -18,7 +18,7 @@ public class main {
 
         System.out.println("Es waren einmal eine Maus, ein Vogel und ein Bratwurst, die lebten ");
         System.out.println("zusammen in einer kleinen Huette am Rande eines verwunschenen Waldes.");
-        System.out.println();
+        System.out.println("In diesem Wald lebt auch ein böses Monster");
 
         /*
          * Zweites Kapitel
